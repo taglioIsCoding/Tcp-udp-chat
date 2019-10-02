118054608cf66cf13900301ae4c36ce1c894361e
Michele Tagliani <45893037+taglioIsCoding@users.noreply.github.com>
2019-10-02 10:52:43 +0200

    First (copied) commit

---
+++ src/tcp_udp/tcp/EchoClient.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tcp_udp.tcp;
+
+/**
+ *
+ * @author 71367370
+ */
+import java.io.*;
+import java.net.*;
+
+public class EchoClient {
+    public static void main(String[] args) throws IOException {
+        
+        String hostName;
+        int portNumber;
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("Insert the ip adress: ");
+        hostName = r.readLine();
+        System.out.println("Insert the port number: ");
+        portNumber = Integer.parseInt(r.readLine());
+        
+ /*       if (args.length != 2) {
+            System.err.println(
+                "Usage: java EchoClient <host name> <port number>");
+            System.exit(1);
+        }*/
+
+        try (
+            Socket echoSocket = new Socket(hostName, portNumber);
+            PrintWriter out =
+                new PrintWriter(echoSocket.getOutputStream(), true);
+            BufferedReader in =
+                new BufferedReader(
+                    new InputStreamReader(echoSocket.getInputStream()));
+            BufferedReader stdIn =
+                new BufferedReader(
+                    new InputStreamReader(System.in))
+        ) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                hostName);
+            System.exit(1);
+        } 
+    }
+}
