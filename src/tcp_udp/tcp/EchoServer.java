54c462116905b95defdaa22ac3f30f0586230816
Michele Tagliani <45893037+taglioIsCoding@users.noreply.github.com>
2019-10-02 11:49:53 +0200

    Now looks like a chat

--- src/tcp_udp/tcp/EchoServer.java
+++ src/tcp_udp/tcp/EchoServer.java
@@ -35,13 +35,17 @@ public class EchoServer {
                 new ServerSocket(/*Integer.parseInt(args[0])*/portNumber);
             Socket clientSocket = serverSocket.accept();     
             PrintWriter out =
-                new PrintWriter(clientSocket.getOutputStream(), true);                   
+                new PrintWriter(clientSocket.getOutputStream(), true);   
             BufferedReader in = new BufferedReader(
                 //new InputStreamReader(clientSocket.getInputStream()));
                     new InputStreamReader(System.in));
+            BufferedReader inC = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
         ) {
             String inputLine;
+            System.out.println("Client: "+ inC.readLine());
             while ((inputLine = in.readLine()) != null) {
+                //System.out.println("Client: "+ inC.readLine());
                 out.println(inputLine);
             }
         } catch (IOException e) {
